3362257ca45fb113394f08422617af2efcfcc68e
Shalini <shalini5640j@gmail.com>
2023-08-05 18:50:17 +0530

    add get collection method

--- lib/database/init.py
+++ lib/database/init.py
@@ -2,6 +2,7 @@ import os
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo.collection import Collection
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,3 +16,9 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+db = client["tt"]
+
+
+def get_collection(collection_name: str) -> Collection:
+    return db[collection_name]
