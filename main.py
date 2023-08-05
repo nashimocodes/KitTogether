bcd09281ae70278de3fa67d235e8bcd67cd35143
codeash24 <o.a.aishu@gmail.com>
2023-08-05 17:58:35 +0530

    bmi

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from taipy.gui import Gui
 
-from pages.chart import CHART_PAGE
+from pages.chart import weightInfo
+
+
+weight = 0
+height = 0
+result = 0
+
+
+def bodyMassIndex(state):
+    result = state.weight / (state.height / 100) ** 2
+    state.assign("result", result)
+
 
 pages = {
-    "chart": CHART_PAGE,
+    "chart": weightInfo,
 }
 
 if __name__ == "__main__":
--- pages/chart.py
+++ pages/chart.py
@@ -1,2 +1,10 @@
-CHART_PAGE = """
+weightInfo = """
+
+### Your weight info needs to be entered.
+
+<|{weight}|number|label=weight(kg)|on_change=bodyMassIndex|>
+<|{height}|number|label=height(cm)|on_change=bodyMassIndex|>
+# <|{date}|date|label=date|>
+<|{submit}|button|label=submit|on_change=bodyMassIndex|>
+<|{result}|text|label=BMI|>
 """
